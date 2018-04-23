321309beb05b24eaf015dd0a9c147874fbbce5e7
puncan <armatisp@oregonstate.edu>
2018-04-23 15:19:18 -0700

    added test function for the htcorrelation function. After some consideration, I will probably not be using correlations after all. This is for practice.

--- flowdist/heattransfer.py
+++ flowdist/heattransfer.py
@@ -1,5 +1,5 @@
 import ht.conv_internal
-import CoolProp.CoolProp.PropsSI as cp
+from CoolProp.CoolProp import PropsSI as cp
 import math
 
 # for now, heat transfer correlations are chosen based on whether the
@@ -11,21 +11,24 @@ import math
 # fully developed flow is also assumed
 
 def choosehtcorr(reynolds, prandtl):
-    if reynolds >= 1 or reynolds <= 10000:
+    if reynolds > 0 and reynolds <= 10000:
         nusselt = ht.conv_internal.laminar_Q_const()
 
-    elif reynolds > 10000 and prandtl >= 0.6 and prandtl >= 160:
+    elif reynolds > 10000 and prandtl >= 0.6 and prandtl <= 160:
         nusselt = ht.conv_internal.turbulent_Dittus_Boelter(reynolds, prandtl, heating = True, revised = False)
 
     else:
-        return 'No Nusselt correlation available for this Prandtl or Reynolds number' + 'Re = ' + str(reynolds) + ', Pr = ' str(prandtl)
+        return 'Unacceptable Reynolds or Prandtl number: ' + 'Re = ' + str(reynolds) + ', Pr = ' + str(prandtl)
 
     return nusselt
 
 
 def temperature_surface(nusselt, conductivity, diameter, temperature_bulk, heatflux):
-    htcoeff_convective = nusselt*conductivity/diameter
-    return (heatflux/htcoeff_convective) + temperature_bulk
+    if nusselt > 0 and conductivity > 0 and diameter > 0 and temperature_bulk > 200:
+        htcoeff_convective = nusselt*conductivity/diameter
+        return (heatflux/htcoeff_convective) + temperature_bulk
+    else:
+        return 'Unacceptable input value(s)'
 
 
 def temperatured_out(fluid, flowrate_mass, diameter, heatflux, temperature_bulk_in, pressure):
