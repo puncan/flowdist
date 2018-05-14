72b672c32ab2cc5530c17d177c725841fec51b31
puncan <armatisp@oregonstate.edu>
2018-05-13 23:08:59 -0700

    Gave up on SymPy for now, writting a class for flow elements to help organize data.

--- flowdist/fluidmechanics.py
+++ flowdist/fluidmechanics.py
@@ -1,9 +1,91 @@
 import numpy as np
-import sympy as sp
+
 
 """Eventually, this module should be able to set up a 3D transient fluid flow problem for a rectangular
 duct for the solver inputs. Currently, only a 2D advection-diffusion problem is to be set up to get
 things rolling. The initial conditions, boundary conditions, and fluid properties will be established here"""
+
+
+class FiniteVolume(object):
+    """A finite volume is a defined space in which energy and mass can enter and leave via several processes including
+    diffusion and advection. In this class, the finite volume is a quadrahedron.
+
+    Attributes
+    __________
+
+    size : length, width, and depth of the quadrahedron in [m]
+    time_step : in [s]
+    position : x, y, and z location in [m]
+    reference : i, j, and k as negative or positive number
+    velocity_estimated : u, v, and w velocities in [m/s]
+    velocity : u, v, and w velocities in [m/s]
+    pressure_estimated : p pressure in [Pa]
+    pressure : p pressure in [Pa]
+    density : fluid density in [kg/m^3]
+    viscosity : in [kg/(m*s)]
+    conductivity : fluid thermal conductivity [W/(m*K)]
+    """
+
+    def __init__(self, size, time_step, position, reference, velocity_estimated, velocity, pressure_estimated, pressure, density,
+                 viscosity, conductivity):
+
+        self.size = size
+        self.time_step = time_step
+        self.position = position
+        self.reference = reference
+        self.velocity_estimated = velocity_estimated
+        self.velocity = velocity
+        self.pressure_estimated = pressure_estimated
+        self.pressure = pressure
+        self.density = density
+        self.viscosity = viscosity
+        self.conductivity = conductivity
+
+    def get_momentum_coefficients(self):
+        """Calculates the time coefficient for all the momentum equations at the current time step and the a
+        coefficients in the x, y, and z directions. The coefficients are dependent on the reference, being the relative
+        location of an adjacent node. Currently, the generalized convection-diffusion scheme is used from Chapter 8 of
+        "Computational Methods for Heat and Mass Transfer" by Pradip Majumdar with the option for adding 5 different
+        approximation schemes. The Hybrid scheme is selected for now."""
+
+        a_m = self.density*self.size['dx']*self.size['dy']*self.size['dz']/self.time_step
+
+        pe_i = self.density*self.velocity_estimated['u']*self.size['dx']/self.viscosity
+        a_pe_i = np.max(np.array([0, 1 - (np.abs(pe_i)/2)]))
+        if self.reference['i'] > 0:
+            a_i = (self.viscosity / self.size['dx'])*a_pe_i + np.max(
+                np.array([0, -self.density*self.velocity_estimated['u']]))
+        elif self.reference['i'] < 0:
+            a_i = (self.viscosity / self.size['dx'])*a_pe_i + np.max(
+                np.array([0, self.density*self.velocity_estimated['u']]))
+        else:
+            print('FlowElement.reference must be a positive or negative number')
+
+        pe_j = self.density*self.velocity_estimated['v']*self.size['dy']/self.viscosity
+        a_pe_j = np.max(np.array([0, 1 - (np.abs(pe_j)/2)]))
+        if self.reference['j'] > 0:
+            a_j = (self.viscosity / self.size['dy'])*a_pe_j + np.max(
+                np.array([0, -self.density*self.velocity_estimated['v']]))
+        elif self.reference['j'] < 0:
+            a_j = (self.viscosity / self.size['dy'])*a_pe_j + np.max(
+                np.array([0, self.density*self.velocity_estimated['v']]))
+        else:
+            print('FlowElement.reference must be a positive or negative number')
+
+        pe_k = self.density*self.velocity_estimated['w']*self.size['dz']/self.viscosity
+        a_pe_k = np.max(np.array([0, 1 - (np.abs(pe_k)/2)]))
+        if self.reference['k'] > 0:
+            a_k = (self.viscosity / self.size['dz'])*a_pe_k + np.max(
+                np.array([0, -self.density*self.velocity_estimated['w']]))
+        elif self.reference['k'] < 0:
+            a_k = (self.viscosity / self.size['dz'])*a_pe_k + np.max(
+                np.array([0, self.density*self.velocity_estimated['w']]))
+        else:
+            print('FlowElement.reference must be a positive or negative number')
+
+        return [a_m, a_i, a_j, a_k]
+
+    
 
 
 def initial_conditions(velocity, grid):
@@ -29,24 +111,4 @@ def initial_conditions(velocity, grid):
                 return v
         except TypeError:
             return em2
-
-
-def momentum_finite_diff_equ():
-    """Returns the symbolic momentum equations for single phase 2D unsteady Newtonian fluid flow.
-    This is intended to eventually return the integrated and discretized form. The built in vector
-    module in sympy will also be used in the future."""
-    x, y, t, dx, dy, dz, dt, u, v, mu, rho, p, su = sp.symbols('x y t dx dy dz dt u v mu rho p su')
-
-    u = sp.Function('u')(x, y, t)
-    v = sp.Function('v')(x, y, t)
-    p = sp.Function('p')(x, y, t)
-    rho = sp.Function('rho')(x, y, t)
-    mu = sp.Function('mu')(x, y, t)
-
-    term_time = sp.Derivative(u, t)
-    term_advection_out = sp.Derivative(rho*u**2, x) + sp.Derivative(rho*u*v, y)
-    term_pressure_grad = sp.Derivative(p, x)
-    term_diffusion = -sp.Derivative(mu*sp.Derivative(u, x), x) - sp.Derivative(mu*sp.Derivative(u, y), y)
-    term_source = -su
-    return term_time + term_advection_out + term_pressure_grad + term_diffusion + term_source
 
--- flowdist/solver.py
+++ flowdist/solver.py
@@ -1,8 +1,32 @@
 import numpy as np
+#from CoolProp.CoolProp import PropsSI
+from heattransfer import initial_conditions as htic
 
 
-def define_grid(length_channel, number_gridpoints):
-    """Returns grid spacing and numpy array containing the axial locations. 1D for now."""
-    grid_spacing = length_channel/(number_gridpoints - 1)
-    positions_axial = np.linspace(0.0, length_channel, num=number_gridpoints)
-    return grid_spacing, positions_axial
+def define_grid(length_channel, height_channel, number_grid_x, number_grid_y):
+    """Returns 2D element dimensions and numpy arrays containing the evenly spaced grid locations along the length and
+    height (x and y)"""
+    dx = length_channel/(number_grid_x - 1)
+    dy = height_channel/(number_grid_y - 1)
+    x = np.linspace(0.0, length_channel, num=number_grid_x)
+    y = np.linspace(0.0, height_channel, num=number_grid_y)
+    return dx, dy, x, y
+
+
+def fluid_properties():
+    '''Returns the fluid density, dynamic viscosity, and thermal conductivity in SI units. Ideally, for the flow field
+    in 2D numpy arrays. These will be dependent on enthalpies (in J/kg), pressures (in Pa), and the fluid type.
+    For now, the properties are hard coded constants for air at ambient conditions.'''
+
+    #density = PropsSI('D', 'P', pressures, 'H', enthalpies, fluid)
+    #viscosity_dynamic = PropsSI('V', 'P', pressures, 'H', enthalpies, fluid)
+    #conductivity = PropsSI('L', 'P', pressures, 'H', enthalpies, fluid)
+
+    return 1.225, 18.45e-6, 0.026
+
+
+#h = PropsSI('H', 'X', 0, 'T', 300, 'air')
+
+rho, mu, k = fluid_properties()
+
+print(rho, mu, k)
